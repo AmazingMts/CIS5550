cf0146daecb0263194d821cf45aade567412651e
Tianshi Miao <mts591227021@gmail.com>
2024-08-30 16:54:46 -0400

    Delete CIS5550/hw0 directory

--- CIS5550/hw0/src/cis5550/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cis5550; 
-public class HelloWorld {
-    public static void main(String[] args) {
-        String content="Hello World (Tianshi Miao)";
-        System.out.println(content);
-    }
-}
