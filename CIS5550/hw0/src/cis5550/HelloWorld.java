3a33172dff2fd0857b2b8abe4b2dd82bcbe6ff9a
Tianshi Miao <mts591227021@gmail.com>
2024-08-30 16:44:24 -0400

    Add files via upload

---
+++ CIS5550/hw0/src/cis5550/HelloWorld.java
@@ -0,0 +1,7 @@
+package cis5550; 
+public class HelloWorld {
+    public static void main(String[] args) {
+        String content="Hello World (Tianshi Miao)";
+        System.out.println(content);
+    }
+}
